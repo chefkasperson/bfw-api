fc7d57c57fea2918acc9717746556fcca9a95a05
chefkasperson <chefkasperson@gmail.com>
2020-04-14 15:55:45 -0700

    added session routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :words
+      resources :children
+      resources :users
+      post '/login', to: 'sessions#new'
+      post '/logout', to: 'sessions#destroy'
+      get '/get_current_user', to: 'sessions#get_current_user'
     end
   end
   
-  namespace :api do
-    namespace :v1 do
-      resources :children
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :users
-    end
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
