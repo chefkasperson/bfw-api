1b339ee0340d489aa0a6dd6fe08cce596df7f139
chefkasperson <chefkasperson@gmail.com>
2020-04-15 18:20:41 -0700

    added login/signin functionality

--- app/controllers/Api/v1/sessions_controller.rb
+++ app/controllers/Api/v1/sessions_controller.rb
@@ -1,11 +1,43 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
 
   def new
+    user = User.find_by(username: params[:user][:username])
+    
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      render json: {
+        current_user: user.as_json(except: [:password_digest], include: [:children]),
+        logged_in: true
+      }
+    else
+      render json: {
+        error: "invalid credentials"
+      }
+    end
+    
+    # params["user"]["username"]
+    # params["user"]["password"]
   end
-
+  
   def destroy
+    reset_session
+    render json: {
+      message: 'successful logout',
+      logged_in: false
+    }
   end
-
-  def get_current_user
+  
+  def get_current_user 
+    if logged_in?
+      render json: {
+        current_user: current_user.as_json(except: [:password_digest], include: [:children]),
+        logged_in: true
+      } 
+    else
+      render json: {
+        message: 'no one logged in',
+        logged_in: false  
+      }
+    end
   end
 end
--- app/controllers/Api/v1/users_controller.rb
+++ app/controllers/Api/v1/users_controller.rb
@@ -15,12 +15,19 @@ class Api::V1::UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      render json: {
+        current_user: user.as_json(except: [:password_digest], include: [:children]),
+        logged_in: true
+      }
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {
+        error: "Unable to create account"
+      }, status: :unprocessable_entity
     end
   end
 
@@ -46,6 +53,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:username, :password)
     end
 end
